742d45ba193b010aa7220062fe39b5b30f6f817d
SnowsSky <exaltypixou@gmail.com>
2024-10-29 17:37:04 +0100

    Add files via upload

---
+++ Clients/config.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+
+module.exports = {
+  intents: [
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageTyping,  
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.Guilds,
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.User,
+  ],
+};
