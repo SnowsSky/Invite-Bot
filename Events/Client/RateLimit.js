e77afda5a4c6034e71c3907091379737c58dea8c
SnowsSky <exaltypixou@gmail.com>
2024-10-29 17:32:45 +0100

    Create RateLimit.js

---
+++ Events/Client/RateLimit.js
@@ -0,0 +1,10 @@
+const { ActivityType, EmbedBuilder } = require("discord.js");
+module.exports = {
+  name: "rateLimit",
+  async execute(rateLimitData, client) {
+      
+  console.error(rateLimitData);
+
+ 
+  }
+}
